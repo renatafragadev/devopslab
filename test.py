167775c9977d3549589fd029b03cc6b58bd508af
Renata dos Santos Fraga <renatafragadev@gmail.com>
2023-06-20 20:42:31 -0300

    adding new endpoint to generate random numbers

--- test.py
+++ test.py
@@ -4,16 +4,24 @@ import unittest
 
 class Test(unittest.TestCase):
     def setUp(self):
-        # cria uma instância do unittest, precisa do nome "setUp"
         self.app = app.test_client()
 
-        # envia uma requisicao GET para a URL
+    def test_root_request(self):
+
         self.result = self.app.get('/')
-
-    def test_requisicao(self):
-        # compara o status da requisicao (precisa ser igual a 200)
+       
         self.assertEqual(self.result.status_code, 200)
-
-    def test_conteudo(self):
-        # verifica o retorno do conteudo da pagina
         self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps")
+
+    def test_randon_request(self):
+        self.result = self.app.get('/random')
+
+        result = int(self.result.data.decode('utf-8'))
+        self.assertEqual(self.result.status_code, 200)
+        self.assertGreaterEqual(result, 1)
+        self.assertLessEqual(result, 20)
+
+
+
+
+
